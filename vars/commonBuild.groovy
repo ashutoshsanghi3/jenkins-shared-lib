5177119f86776911a212580f407233b0a0996be8
Ashutosh Sanghi <ashutoshsanghi3@gmail.com>
2025-04-09 16:11:13 +0530

    groovy

--- vars/commonBuild.groovy
+++ vars/commonBuild.groovy
@@ -1,15 +1,4 @@
 def call() {
-    stage('Checkout') {
-        checkout scm
-    }
-
-    stage('Unit Tests') {
-        echo 'Running unit tests...'
-        sh './gradlew test'
-    }
-
-    stage('Deploy') {
-        echo "Deploying to environment..."
-        // Add deployment logic here, or take it as a parameter
-    }
+    echo "Running common build without parameters"
+    sh 'echo Building the application...'
 }
